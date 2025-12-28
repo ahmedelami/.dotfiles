600d04af7c8b5d50bee66f31ce81dab33294869f
ahmedelami <elaminahmed03@gmail.com>
2025-12-27 21:01:51 -0500

    feat(nvim): add CTRL+k for project file search and enable telescope keybindings

--- .config/nvim/lua/humoodagen/lazy/telescope.lua
+++ .config/nvim/lua/humoodagen/lazy/telescope.lua
@@ -8,6 +8,7 @@ return {
     },
 
     config = function()
+        local builtin = require('telescope.builtin')
         require('telescope').setup({
             defaults = {
                 file_ignore_patterns = {
@@ -17,34 +18,34 @@ return {
             }
         })
 
-        -- local builtin = require('telescope.builtin')
-        -- vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        -- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        -- vim.keymap.set('n', '<leader>pws', function()
-        --     local word = vim.fn.expand("<cword>")
-        --     builtin.grep_string({ search = word })
-        -- end)
-        -- vim.keymap.set('n', '<leader>pWs', function()
-        --     local word = vim.fn.expand("<cWORD>")
-        --     builtin.grep_string({ search = word })
-        -- end)
-        -- vim.keymap.set('n', '<leader>ps', function()
-        --     builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        -- end)
-        -- vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        vim.keymap.set('n', '<C-k>', builtin.find_files, { desc = "Project File Search" })
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>pws', function()
+            local word = vim.fn.expand("<cword>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>pWs', function()
+            local word = vim.fn.expand("<cWORD>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>ps', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
+        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
         
         -- Go to file under cursor with Telescope (better for complex paths)
-        -- vim.keymap.set('n', '<leader>gf', function()
-        --     local word = vim.fn.expand("<cfile>")
-        --     builtin.find_files({ default_text = word })
-        -- end, { desc = "Find file under cursor" })
+        vim.keymap.set('n', '<leader>gf', function()
+            local word = vim.fn.expand("<cfile>")
+            builtin.find_files({ default_text = word })
+        end, { desc = "Find file under cursor" })
         
         -- Search for visually selected text (simpler approach)
-        -- vim.keymap.set('v', '<leader>ps', function()
-        --     -- Exit visual mode and get the selected text
-        --     vim.cmd('normal! "vy')
-        --     local selected_text = vim.fn.getreg('v')
-        --     builtin.grep_string({ search = selected_text })
-        -- end, { desc = "Search selected text" })
+        vim.keymap.set('v', '<leader>ps', function()
+            -- Exit visual mode and get the selected text
+            vim.cmd('normal! "vy')
+            local selected_text = vim.fn.getreg('v')
+            builtin.grep_string({ search = selected_text })
+        end, { desc = "Search selected text" })
     end
 }
